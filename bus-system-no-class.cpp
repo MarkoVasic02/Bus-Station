d2acac6d52561c438ba0845a57e74439fcbff93a
MarkoVasic02 <vasicmarko390@gmail.com>
2020-07-20 12:43:55 +0200

    Implemented Class and Fixed Few Things

--- bus-system-no-class.cpp
+++ bus-system-no-class.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 
-    void choose();
-
+class BusStation
+{
     // int variables
     int choice;
     int wanted_bus;
@@ -26,13 +26,33 @@ using namespace std;
 
     // counters
     int i = 0;
-    int k = 0;
-    int p = 0;
-    int y = 0;
-    int z = 0;
+    int x = 0;
+
+public:
+
+    BusStation()
+    {
+        for(i=0; i<50; i++)
+            bus_no[i] = 0;
+
+        for(i=0; i<32; i++)
+            seat_nums_av[i] = 0;
+    }
+
+    void choose();
+
+    void install();
+
+    void reservation();
+
+    void avail_bss();
+
+    void res_info();
+
+};
 
     // function that provides the possibility to register new bus
-    void install()
+    void BusStation :: install()
     {
         // loop that checks if there is a free space for another bus
         // and ads input to the array if there is one.
@@ -42,6 +62,7 @@ using namespace std;
 
         // If the bus number already exists, it returns notification
         // related to that
+        i=0;
         while(true)
         {
             if(bus_no[i] == NULL || bus_no[i] == 0)
@@ -49,10 +70,7 @@ using namespace std;
                 cout << "Enter new bus number: ";
                 cin >> bus_no[i];
 
-                if(bus_no[i] == -1)
-                {
-                    choose();
-                }
+                if(bus_no[i] == -1) choose();
                 break;
             }
             else
@@ -74,43 +92,28 @@ using namespace std;
         cout << "Enter drivers name: ";
         cin >> driver_name;
 
-        if(driver_name == "-1")
-        {
-            choose();
-        }
+        if(driver_name == "-1") choose();
 
         cout << "Enter arrival time: ";
         cin >> arrival_time;
 
-        if(arrival_time == "-1")
-        {
-            choose();
-        }
+        if(arrival_time == "-1") choose();
 
         cout << "Enter departure time: ";
         cin >> departure_time;
 
-        if(departure_time == "-1")
-        {
-            choose();
-        }
+        if(departure_time == "-1") choose();
 
         cout << "From: ";
         cin >> from;
 
-        if(from == "-1")
-        {
-            choose();
-        }
+        if(from == "-1") choose();
 
         cout << "To: ";
         cin >> to;
         cout << endl;
 
-        if(to == "-1")
-        {
-            choose();
-        }
+        if(to == "-1") choose();
 
         cout << stars << endl << endl;
     }
@@ -118,7 +121,7 @@ using namespace std;
     // Providing user the possibility
     // to make a reservation based on
     // the available buses that exist
-    void reservation()
+    void BusStation :: reservation()
     {
         // For every of the 50 times which
         // user has to try entering valid number,
@@ -128,7 +131,7 @@ using namespace std;
 
         // User also have the possibility to
         // exit by providing -1 as a command
-        for(int k = 0; k < 50; k++)
+        for(x=0; x < 50; x++)
         {
             cout << "Enter wanted bus number: \n";
             cin >> wanted_bus;
@@ -138,11 +141,11 @@ using namespace std;
                 wanted_bus = NULL;
                 choose();
             }
-            for(int p = 0; p < 50; p++)
+            for(i = 0; i < 50; i++)
             {
-                if(wanted_bus == bus_no[p])
+                if(wanted_bus == bus_no[i])
                 {
-                    k = 54;
+                    x = 54;
                 }
             }
         }
@@ -158,7 +161,7 @@ using namespace std;
 
         // If invalid value is entered,
         // there is notification popping up
-        for(int y = 0; y < 50; y++)
+        for(x = 0; x < 50; x++)
         {
             cout << "Enter seat number: \n";
             cin >> seat_num;
@@ -166,17 +169,19 @@ using namespace std;
             if(seat_num == -1)
             {
                 choose();
+                wanted_bus = NULL;
+                seat_num = NULL;
             }
             else
             {
-                if(seat_num < 32 && seat_num > 0)
+                if(seat_num < 33 && seat_num > 0)
                 {
-                    for(int z = 0; z < 32; z++)
+                    for(i = 0; i < 32; i++)
                     {
-                        if(seat_nums_av[z] == NULL)
+                        if(seat_nums_av[i] == NULL)
                         {
-                            seat_nums_av[z] = seat_num;
-                            y = 54;
+                            seat_nums_av[i] = seat_num;
+                            x = 54;
                         }
                     }
                 }
@@ -192,13 +197,17 @@ using namespace std;
 
         if(passanger_name == "-1")
         {
+            passanger_name = "";
+            seat_num = NULL;
+            wanted_bus = NULL;
             choose();
         }
+
     }
 
     // Displaying all the
     // available buses
-    void avail_bss()
+    void BusStation :: avail_bss()
     {
         for(i=0; i<50; i++)
         {
@@ -210,9 +219,7 @@ using namespace std;
             else
             {
                 if(i == 49 && total < 1)
-                {
-                    cout << "\n~There are no available buses, sorry~\n\n";
-                }
+                cout << "\n~There are no available buses, sorry~\n\n";
             }
         }
     }
@@ -221,13 +228,13 @@ using namespace std;
     // Two cases:
     // - reservation false
     // - reservation true
-    void res_info()
+    void BusStation :: res_info()
     {
         cout << stars << endl << endl;
 
         cout << "Reservation Information:\n\n";
 
-        if(wanted_bus == 0)
+        if(wanted_bus < 1 || wanted_bus == NULL)
         {
             cout << "Bus Number:     ";
             cout << "   Driver: ";
@@ -255,28 +262,20 @@ using namespace std;
         for(i=1; i < 33; i++)
         {
             // FIX NEEDED - instead of having passengers name only next to the seat
-            // which is reserved by him, there is a name next to every seat.........
-            state = (seat_nums_av[i-1] == 0) ? state = "Empty" : state = passanger_name;
+            // reserved by him, there is a name next to every seat.........
+            state = (seat_nums_av[i-1] == 0) ? "Empty" : passanger_name;
 
             cout << i << ". " << state << "   ";
 
             if(i==4 || i==8 || i==12 || i==16 || i==20 || i==24 || i==28 || i==32)
-            {
-                cout << endl << endl;
-            }
+            cout << endl << endl;
         }
-    }
-
-    // :0
-    void quit()
-    {
-        exit(0);
     }
 
     // Only Function We Are Calling
     // main menu which is calling
     // the rest of the functions
-    void choose()
+    void BusStation :: choose()
     {
 
         while(choice!=5)
@@ -312,7 +311,7 @@ using namespace std;
                     break;
 
                 case 5:
-                    quit();
+                    exit(0);
                     break;
             }
         }
@@ -320,5 +319,6 @@ using namespace std;
 
 int main()
 {
-    choose();
+    BusStation station;
+    station.choose();
 }
